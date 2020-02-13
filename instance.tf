8d0b6ee6a6552b0951f5bf499990229c57ac1dea
satinderthakur <59055953+satinderthakur@users.noreply.github.com>
2020-02-13 21:16:17 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -7,20 +7,4 @@ resource "aws_instance" "example_1" {
   instance_type = "t2.micro"
   }
 
-  # Public Key SSH
-  key_name = aws_key_pair.mykeypair.key_name
 
-  # VPC Subnet
-  subnet_id = aws_subnet.test-public-1.id
-
-  # Security Group
-  vpc_security_group_ids = [aws_security_group.allow-ssh.id]
-
-  #User Data
-  user_data = file("userData.sh")
-
-  tags = {
-    Name = "Terraform Instance"
-    Class = "Test"
-  }
-}
