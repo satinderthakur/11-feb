334eb869691f387911c6d92e5ae323b085b29f0e
satinderthakur <59055953+satinderthakur@users.noreply.github.com>
2020-02-13 21:10:46 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -2,7 +2,7 @@ provider "aws" {
   profile    = "default"
   region     = "ap-south-1"
 }
-resource "aws_instance" "example" {
+resource "aws_instance" "example_1" {
   ami           = "ami-2757f631"
   instance_type = "t2.micro"
   }
