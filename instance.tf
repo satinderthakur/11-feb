5ca06ab47339d5d29243c5fdc4aa219065c99e9d
satinderthakur <59055953+satinderthakur@users.noreply.github.com>
2020-02-14 09:33:30 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,10 +1,21 @@
-provider "aws" {
-  profile    = "default"
-  region     = "ap-south-1"
-}
-resource "aws_instance" "example_1" {
-  ami           = "ami-2757f631"
-  instance_type = "t2.micro"
+resource "aws_instance" "my_instance_1"{
+  ami = var.AMIS[var.AWS_REGION]
+  instance_type = var.AWS_INSTANCE
+
+  # Public Key SSH
+  key_name = aws_key_pair.mykeypair.key_name
+
+  # VPC Subnet
+  subnet_id = aws_subnet.test-public-1.id
+
+  # Security Group
+  vpc_security_group_ids = [aws_security_group.allow-ssh.id]
+
+  #User Data
+  user_data = file("userData.sh")
+
+  tags = {
+    Name = "Terraform Instance"
+    Class = "Test"
   }
-
-
+}
