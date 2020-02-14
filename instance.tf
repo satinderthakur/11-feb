f43c989223bca4212321187d6be317de826e83f9
satinderthakur <59055953+satinderthakur@users.noreply.github.com>
2020-02-14 12:39:29 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -11,11 +11,29 @@ resource "aws_instance" "my_instance_1"{
   # Security Group
   vpc_security_group_ids = [aws_security_group.allow-ssh.id]
 
-  #User Data
-  user_data = file("userData.sh")
+  # Provisoner
+  provisioner "remote-exec"{
+    connection{
+      type = "ssh"
+      host = self.public_ip
+      private_key = file("D:\Activity\Newfolder\ec2-key")
+      user = "ec2-user"
+    }
+    inline = [
+      "sudo yum update -y",
+      "sudo amazon-linux-extras install docker -y",
+      "sudo service docker start",
+      "sudo usermod -a -G docker ec2-user",
+      "sudo service docker enable",
+      "sudo $(aws ecr get-login --no-include-email --region ap-south-1)",
+      "sudo docker pull 506844237526.dkr.ecr.ap-south-1.amazonaws.com/demo:latest",
+      "sudo docker run -itd -p 8080:8080 506844237526.dkr.ecr.ap-south-1.amazonaws.com/demo:latest",
+    ]
+  }
 
   tags = {
     Name = "Terraform Instance"
     Class = "Test"
   }
-}
+} 
+ 
